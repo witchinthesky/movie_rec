e1ccebb8f70406253f28fd1d6234c82d821817e0
witchinthesky <79980577+witchinthesky@users.noreply.github.com>
2023-11-12 15:32:18 +0200

    allow all host

--- movie_rec/settings.py
+++ movie_rec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#=#0e))hfb0**8#azx0&=&7#uqv-us-z-nb2!-)qez5+59)!nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
